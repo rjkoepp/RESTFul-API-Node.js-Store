264646171f048b3b890e495437b4ff8714a15c88
rjkoepp <rchangko@ucsd.edu>
2018-06-25 16:06:51 -0700

    added error handling

--- app.js
+++ app.js
@@ -2,16 +2,37 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); // funnel all requests thru morgan, log requests
 
 const productRoutes = require('./api/routes/products'); // takes to file !!!
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 // anything starting with /products in the url is forward to products.js
-app.use('/products', productRoutes);
+app.use('/products', productRoutes); // middleware
+app.use('/orders', orderRoutes); // middleware
 
-app.use('/orders', orderRoutes);
+// anything that made it past the two middlewares, no routes exist
 
+app.use((req, res, next) => {
 
+    const error = new Error('Not found');
+    error.status = 404; // no route found
+    next(error); // forward error in place of original request 
+}); // 'use' sets up middle ware thru which incoming requests go thru
+
+// handles all errors thrown from anywhere in application
+app.use((error, req, res, next ) => {
+
+    res.status(error.status || 500);
+    res.json ({
+        error: {
+            message: error.message
+        }
+    });
+
+}); 
 module.exports = app;
 
 
