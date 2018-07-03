e9587d33331edca4aca039e898a7f4b6b10cc2e7
rjkoepp <rchangko@ucsd.edu>
2018-07-03 13:56:13 -0700

    update product schema

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId, // serial id string (for mongoose)
-    name: String,
-    price: Number
+    name: { type: String, required: true },
+    price: { type: Number, required: true }
 }); // layout of object
 
 // model is the object itself (constructor)
